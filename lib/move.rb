e45c462be7c9f778b90a470b7ba7dd2a50820035
jolly-technologist-1233 <pbrennanmusic@gmail.com>
2021-03-06 21:12:48 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -11,8 +11,5 @@ def input_to_index(input)
 end
 
 def move(board, index, value = "X")
-  board=[" "," "," "," "," "," "," "," "," "]
-  def update_board(board, index, value)
-    board[index]=value
-  end
+  board[index]=value
 end
